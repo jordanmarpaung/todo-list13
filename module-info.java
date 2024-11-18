b38133a898142203e2460dec2862f01e08908002
Jordan <2381025@unai.edu>
2024-11-19 00:08:39 +0700

    handson10

---
+++ module-info.java
@@ -0,0 +1,16 @@
+module TodoListApp {
+    requires java.sql;
+    requires spring.beans;
+    requires spring.context;
+    requires java.xml.crypto;
+    requires org.slf4j;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens todoapp;
+    opens todoapp.entities;
+    opens todoapp.repositories;
+    opens todoapp.services;
+    opens todoapp.views;
+    opens todoapp.config;
+}
